da40fc7328dbab5726905d146d399b74dd6627bc
paco111111 <raul.buenagagarcia@iesmiguelherrero.com>
2025-05-01 19:35:15 +0200

    a

---
+++ GestorActividades/src/main/java/Clases/AccesoBaseDatos.java
@@ -0,0 +1,92 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Clases;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+/**
+ *
+ * @author  Raúl Buenaga
+ */
+public class AccesoBaseDatos {
+    
+     private Connection conn = null;
+    // constantes a definir
+    private static final String BD = "";
+    private static final String USUARIO = "";
+    private static final String CLAVE = "root";
+    private static final String URL = "jdbc:mysql://localhost:3306/" + BD;
+
+    
+    /**
+     * Constructor privado que crea una instancia para la conexión con la base de datos.
+     * Utiliza el patrón Singleton para instanciar la clase una unica vez. 
+     */
+    private AccesoBaseDatos() {
+
+        try {
+            Properties properties = new Properties();
+            properties.setProperty("user", USUARIO);
+            properties.setProperty("password", CLAVE);
+            properties.setProperty("useSSL", "false");
+            properties.setProperty("autoReconnect", "true");
+            conn = (Connection) DriverManager.getConnection(URL, properties);
+            if (conn == null) {
+                System.out.println("Error en conexion");
+            } else {
+                System.out.println("Conexion correcta a: " + URL);
+            }
+        } catch (SQLException ex) {
+            // handle any errors
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+        }
+    }
+
+    /**
+     * Este método devuelva la instancia para la conexión. 
+     * @return La instancia AccesoBaseDatos.
+     */
+    public static AccesoBaseDatos getInstance() {
+        return AccesoBaseDatosHolder.INSTANCE;
+
+    }
+    
+    /**
+     * Clase privada que guarda la unica instancia de conexión
+     */
+
+    private static class AccesoBaseDatosHolder {
+
+        private static final AccesoBaseDatos INSTANCE = new AccesoBaseDatos();
+    }
+
+    
+    public Connection getConn() {
+        return conn;
+    }
+
+    /**
+     * Metodo que cierra la conexión con la base de datos.
+     * @return Boolean true si la conexion se ha cerrado, false en caso contrario.
+     */
+    public boolean cerrar() {
+        boolean siCerrada = false;
+        try {
+            conn.close();
+            if (conn.isClosed()) {
+                siCerrada = true;
+            }
+        } catch (SQLException sqe) {
+            System.out.println("Se produjo un error en el cierre");
+        }
+        return siCerrada;
+    }
+
+}
---
+++ GestorActividades/src/main/java/Clases/Utilidad.java
@@ -0,0 +1,95 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Clases;
+
+import java.time.DateTimeException;
+import java.time.LocalDate;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Utilidad {
+    
+    public static String nextString(String texto) {
+        System.out.println(texto);
+        return new Scanner(System.in).nextLine();
+    }
+
+    public static int nextInt(String texto) {
+        boolean salir = false;
+        int temp = 0;
+        do {
+            try {
+                System.out.println(texto);
+                temp = new Scanner(System.in).nextInt();
+                salir = true;
+            } catch (InputMismatchException a) {
+                System.out.println("Error: no se ha introducido un entero");
+            }
+
+        } while (!salir);
+        return temp;
+    }
+
+    public static double nextDouble(String texto) {
+        boolean salir = false;
+        double temp = 0;
+        do {
+            try {
+                System.out.println(texto);
+                temp = new Scanner(System.in).nextDouble();
+                salir = true;
+            } catch (InputMismatchException a) {
+                System.out.println("Error: no se ha introducido un numero");
+            }
+        } while (!salir);
+
+        return temp;
+    }
+    
+    public static boolean nextBolean(String cadena) {
+        boolean temp = false;
+        boolean salir = false;
+        Scanner teclado = new Scanner(System.in);
+        String x;
+        do {
+            try {
+                System.out.println(cadena + " (Si/No)");
+                System.out.println();
+                x = teclado.nextLine();
+                if (x.equalsIgnoreCase("Si") || x.equalsIgnoreCase("S")) {
+                    temp = true;
+                    salir = true;
+                } else if (x.equalsIgnoreCase("No") || x.equalsIgnoreCase("N")) {
+                    temp = false;
+                    salir = true;
+                } else {
+                    throw new Exception();
+                }
+            } catch (Exception e) {
+                System.out.println("Error: respuesta no valida");
+            }
+        } while (!salir);
+        return temp;
+    }
+    
+
+    public static LocalDate fecha(String texto) {
+        System.out.print(texto);
+        int dia = nextInt("Introduce el dia: ");
+        int mes = nextInt("Introduce el mes: ");
+        int anio = nextInt("Introduce el año: ");
+        LocalDate temp = LocalDate.now();
+        try {
+            temp = LocalDate.of(dia, mes, anio);
+        } catch (DateTimeException a) {
+            System.out.println("Error: Los datos no son validos para el formato de fecha");
+        }
+        return temp;
+    }
+}
